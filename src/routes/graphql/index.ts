0ffa63fadb3175cda50033779af79dd20317463e
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-07-23 19:59:49 +0300

    feat: implement depthLimit()

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLError, validate, parse } from 'graphql';
 import { schema } from './types/schemas.js';
 import memberTypeResolver from './resolvers/memberTypeResolver.js';
+import depthLimit from 'graphql-depth-limit';
 
 const rootValue = memberTypeResolver;
 
@@ -17,6 +18,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const errors: readonly GraphQLError[] = validate(
+        schema, parse(req.body.query), [depthLimit(5)]
+      );
+      if (errors.length) return { errors };
+
       const result = await graphql({
         schema,
         source: req.body.query,
